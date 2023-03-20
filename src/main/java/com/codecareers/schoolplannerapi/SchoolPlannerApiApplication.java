5cfaacdd05d19d8ef0c143dde2cd61d60a93f7c2
Venus Velazquez <venus.dv10@gmail.com>
2023-03-19 22:00:01 -0400

    Setting up project structure

---
+++ src/main/java/com/codecareers/schoolplannerapi/SchoolPlannerApiApplication.java
@@ -0,0 +1,13 @@
+package com.codecareers.schoolplannerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPlannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolPlannerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecareers/schoolplannerapi/SchoolPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecareers.schoolplannerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolPlannerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
